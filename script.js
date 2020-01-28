58f71e01442a171781f0273cbd3f6af684a1a402
Hithalo Kildaree Santos da Costa <hcosta@petros.com.br>
2020-01-28 15:24:12 -0300

    imc calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputAltura = document.getElementById('altura');
+const inputPeso = document.getElementById('peso');
+const divBox = document.getElementById('box');
+const textImc = document.createElement('h4');
+const imcValue = document.createElement('h2');
+const btn = document.getElementById('btn-calc');
+
+const calcularImc = (altura, peso) => {
+    const imc = peso / (altura * altura);
+    return parseFloat(imc.toFixed(1));
+}
+
+const verificarImc = (altura, peso) => {
+    const imc = calcularImc(altura, peso);
+    imcValue.innerText = `Seu IMC: ${imc}`;
+    divBox.append(imcValue);
+    if (imc >= 30) {
+        textImc.innerText = 'Você está acima do peso!';
+        textImc.classList.add('text-center')
+        return divBox.append(textImc);
+    } else if (imc >= 18 || imc == 24) {
+        textImc.innerText = 'Seu peso está normal!';
+        textImc.classList.add('text-center')
+        return divBox.append(textImc)
+    } else if (imc < 18) {
+        textImc.innerText = 'Você está abaixo do peso!';
+        textImc.classList.add('text-center')
+        return divBox.append(textImc)
+    }
+}
+
+btn.addEventListener('click', () => {
+    verificarImc(+inputAltura.value, +inputPeso.value);
+})
