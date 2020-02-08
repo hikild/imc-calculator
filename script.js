7eaa404cbed34c9ffbf3d2f39780398acf77546f
hikild <kildaree02@gmail.com>
2020-02-07 22:06:18 -0300

    imc calculator

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
 const inputAltura = document.getElementById('altura');
 const inputPeso = document.getElementById('peso');
 const divBox = document.getElementById('box');
@@ -6,29 +8,31 @@ const imcValue = document.createElement('h2');
 const btn = document.getElementById('btn-calc');
 
 const calcularImc = (altura, peso) => {
-    const imc = peso / (altura * altura);
-    return parseFloat(imc.toFixed(1));
-}
+  const imc = peso / (altura * altura);
+  return parseFloat(imc.toFixed(1));
+};
 
 const verificarImc = (altura, peso) => {
-    const imc = calcularImc(altura, peso);
-    imcValue.innerText = `Seu IMC: ${imc}`;
-    divBox.append(imcValue);
-    if (imc >= 30) {
-        textImc.innerText = 'Você está acima do peso!';
-        textImc.classList.add('text-center')
-        return divBox.append(textImc);
-    } else if (imc >= 18 || imc == 24) {
-        textImc.innerText = 'Seu peso está normal!';
-        textImc.classList.add('text-center')
-        return divBox.append(textImc)
-    } else if (imc < 18) {
-        textImc.innerText = 'Você está abaixo do peso!';
-        textImc.classList.add('text-center')
-        return divBox.append(textImc)
-    }
-}
+  const imc = calcularImc(altura, peso);
+  imcValue.innerText = `Seu IMC: ${imc}`;
+  divBox.append(imcValue);
+  if (imc >= 30) {
+    textImc.innerText = 'Você está acima do peso!';
+    textImc.classList.add('text-center');
+    return divBox.append(textImc);
+  }
+  if (imc >= 18 || imc === 24) {
+    textImc.innerText = 'Seu peso está normal!';
+    textImc.classList.add('text-center');
+    return divBox.append(textImc);
+  }
+  if (imc < 18) {
+    textImc.innerText = 'Você está abaixo do peso!';
+    textImc.classList.add('text-center');
+    return divBox.append(textImc);
+  }
+};
 
 btn.addEventListener('click', () => {
-    verificarImc(+inputAltura.value, +inputPeso.value);
-})
+  verificarImc(+inputAltura.value, +inputPeso.value);
+});
